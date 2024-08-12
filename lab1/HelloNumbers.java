4ffb24fb81e0f4806f338f0c0424f67b1957ea79
Zihan He <hezihan745@gmail.com>
2024-08-12 23:46:15 +0800

    completes first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,16 @@
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int x = 0;
+		int sum = 0;
+		while (x < 10) {
+			sum = sum + x;
+			System.out.print(sum + " ");
+			x = x + 1;
+		}
+		System.out.println("");
+		String h = 5 + "horse";
+		System.out.println(h);
+		System.out.println(5 + "10");
+		System.out.println(5 + 10);
+	}
+}
