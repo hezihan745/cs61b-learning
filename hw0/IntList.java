096d0677c608bdba985e53bc27da17d90fbef105
Zihan HE <hezihan745@gmail.com>
2024-08-18 21:42:54 +0800

    Implement the size method of IntList

---
+++ hw0/IntList.java
@@ -0,0 +1,24 @@
+public class IntList {
+    public int first;
+    public IntList rest;
+
+    public IntList (int f, IntList r) {
+        first = f;
+        rest = r;
+    }
+
+    public int size() {
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + rest.size();
+    }
+
+    public static void main(String[] args) {
+        IntList L = new IntList(5, null);
+        L = new IntList(10, L);
+        L = new IntList(15, L);
+
+        System.out.println(L.size());
+    }
+}
